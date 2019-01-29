a5e43b549ded97e8588d48ce30134bbe69d6e14b
Michael Kennel <mkennel@southbendin.gov>
2019-01-29 16:43:08 -0500

    adding redirects for authentication, something screwing up, need to check links

--- src/main.js
+++ src/main.js
@@ -1,23 +1,32 @@
-import Vue from "vue";
 import firebase from 'firebase';
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
 
+let app = '';
+
 // Initialize Firebase
-const config = {
+firebase.initializeApp({
   apiKey: "AIzaSyBI8i77wNk29SoIBujv0goC9FKq5cY6wnc",
   authDomain: "fir-proof-of-concept-a6fc1.firebaseapp.com",
   databaseURL: "https://fir-proof-of-concept-a6fc1.firebaseio.com",
   projectId: "fir-proof-of-concept-a6fc1",
   storageBucket: "fir-proof-of-concept-a6fc1.appspot.com",
   messagingSenderId: "768522867112"
-};
+});
 
-firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h =>(App)
+    }).$mount('#app');
+  }
+});
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount("#app");
