60ada1289bbd7e53e04aef8a66ded32d482b5570
Michael Kennel <mkennel@southbendin.gov>
2019-01-29 15:40:30 -0500

    added firebase, cleaned up repo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
+import firebase from 'firebase';
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBI8i77wNk29SoIBujv0goC9FKq5cY6wnc",
+  authDomain: "fir-proof-of-concept-a6fc1.firebaseapp.com",
+  databaseURL: "https://fir-proof-of-concept-a6fc1.firebaseio.com",
+  projectId: "fir-proof-of-concept-a6fc1",
+  storageBucket: "fir-proof-of-concept-a6fc1.appspot.com",
+  messagingSenderId: "768522867112"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
