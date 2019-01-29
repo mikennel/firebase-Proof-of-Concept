54ef41e2c71b7216cf0c33ccd897d135019a775c
Michael Kennel <mkennel@southbendin.gov>
2019-01-29 15:05:37 -0500

    implementing vue, messing with functionality

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <label for="uname">Username</label><br>
+        <input type="text" placeholder="Username" name="uname" required><br>
+        <label for="pword">Password</label><br>
+        <input type="password" placeholder="Password" name="pword" required><br>
+        <button @click="login">Connection</button><br>
+        <p>Don't have an account?  <router-link to="/sign-up">Create one</router-link></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return{};
+        },
+        methods: {
+            login: function(){
+                this.$router.replace('home');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login{
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    p {
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="signUp">
+        <h3>Create New Account</h3>
+        <label for="uname">Email</label><br>
+        <input type="email" placeholder="Email" name="uname" required><br>
+        <label for="pword">Password</label><br>
+        <input type="password" placeholder="Password" name="pword" required><br>
+        <button>Sign Up</button><br>
+        <span>Go back to <router-link to="/login">login.</router-link></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'signUp',
+        data() {
+            return{};
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+    .signUp{
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span{
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
